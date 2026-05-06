5ea4d599cae1dc2756f066f298313eaef8e271de
ahadullabaig <ahadullabaig.16@gmail.com>
2026-05-06 21:27:44 +0530

    modified question 2a

--- q2/2a.cpp
+++ q2/2a.cpp
@@ -6,16 +6,16 @@ using namespace std;
 void proveBigOmega()
 {
     int c = 10;
-
     int n0 = 1;
 
     cout << left << setw(10) << "n" 
          << setw(20) << "f(n)=3n^2+4n+3" 
          << setw(15) << "c*g(n)=10n" 
          << "f(n) >= c*g(n)?" << endl;
+
     cout << "------------------------------------------------------------" << endl;
 
-    for (int n = 1; n <= 10; ++n)
+    for(int n = 1; n <= 10; ++n)
     {
         long fn = 3 * n * n + 4 * n + 3;
         long cgn = c * n;
