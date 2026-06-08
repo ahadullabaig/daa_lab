46114d9b3091cf98fc42954e932f33e00e9b95a5
ahadullabaig <ahadullabaig.16@gmail.com>
2026-06-09 02:49:08 +0530

    modified 10b.cpp

--- q10/10b.cpp
+++ q10/10b.cpp
@@ -11,43 +11,40 @@ struct Request {
     int value;
 };
 
-bool compareRequests(Request a, Request b) {
+bool compareRequests(Request a, Request b)
+{
     return a.finish < b.finish;
 }
 
-// Find the latest job (in sorted array) that doesn't conflict with job[i]
-int latestNonConflict(const vector<Request>& reqs, int i) {
-    for (int j = i - 1; j >= 0; j--) {
-        if (reqs[j].finish <= reqs[i].start)
-            return j;
+int latestNonConflict(const vector<Request>& reqs, int i)
+{
+    for (int j = i - 1; j >= 0; j--)
+    {
+        if (reqs[j].finish <= reqs[i].start) return j;
     }
+
     return -1;
 }
 
-void maxProfit(vector<Request>& reqs) {
+void maxProfit(vector<Request>& reqs)
+{
     sort(reqs.begin(), reqs.end(), compareRequests);
     
     int n = reqs.size();
+    
     vector<int> dp(n);
-    vector<int> choice(n, -1);
     
     dp[0] = reqs[0].value;
     
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < n; i++)
+    {
         int inclProfit = reqs[i].value;
+
         int l = latestNonConflict(reqs, i);
-        if (l != -1) {
-            inclProfit += dp[l];
-        }
+    
+        if (l != -1) inclProfit += dp[l];
         
-        if (inclProfit > dp[i - 1]) {
-            dp[i] = inclProfit;
-            choice[i] = l;
-        } else {
-            dp[i] = dp[i - 1];
-            choice[i] = i - 1; // Actually this just maps to DP state not forming current
-            // We need a better way to trace back, but let's just print max profit.
-        }
+        dp[i] = max(inclProfit, dp[i - 1]);
     }
     
     cout << "Maximum profit for the drama venue: " << dp[n - 1] << endl;
