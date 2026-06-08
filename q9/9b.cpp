e5a6d57b0c71e04f0e216341212298847412a84c
ahadullabaig <ahadullabaig.16@gmail.com>
2026-06-09 02:33:20 +0530

    modified 9b.cpp

--- q9/9b.cpp
+++ q9/9b.cpp
@@ -5,35 +5,33 @@ using namespace std;
 
 void knapsack(int W, vector<int>& weight, vector<int>& value, int n)
 {
-    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+    vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
 
-    // Build table dp[][] in bottom up manner
     for (int i = 1; i <= n; i++)
     {
         for (int w = 1; w <= W; w++)
         {
-            if (weight[i - 1] <= w)
-                dp[i][w] = max(value[i - 1] + dp[i - 1][w - weight[i - 1]], dp[i - 1][w]);
+            if (weight[i-1] <= w)
+                dp[i][w] = max(value[i-1] + dp[i-1][w - weight[i-1]], dp[i-1][w]);
             else
-                dp[i][w] = dp[i - 1][w];
+                dp[i][w] = dp[i-1][w];
         }
     }
 
-    int res = dp[n][W];
-    cout << "Maximum value accommodated: " << res << endl;
+    cout << "Maximum value accommodated: " << dp[n][W] << endl;
     cout << "Items selected: ";
 
     int w = W;
-    for (int i = n; i > 0 && res > 0; i--)
+    
+    for (int i = n; i > 0; i--)
     {
-        if (res == dp[i - 1][w]) 
-            continue;    
-        else {
-            cout << "Item " << i << " (Weight: " << weight[i - 1] << ", Value: " << value[i - 1] << ") ";
-            res -= value[i - 1];
-            w -= weight[i - 1];
+        if (dp[i][w] != dp[i-1][w])
+        {
+            cout << "Item " << i << " (Weight: " << weight[i-1] << ", Value: " << value[i-1] << ") ";
+            w -= weight[i-1];
         }
     }
+
     cout << endl;
 }
 
@@ -41,7 +39,9 @@ int main()
 {
     vector<int> weight = {3, 5, 6, 2};
     vector<int> value = {10, 4, 9, 11};
+
     int W = 7;
+    
     int n = value.size();
 
     knapsack(W, weight, value, n);
