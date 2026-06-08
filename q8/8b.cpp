8b31e92cf00e9f0f460900146443949504ead232
ahadullabaig <ahadullabaig.16@gmail.com>
2026-06-08 17:00:32 +0530

    modified 8b.cpp

--- q8/8b.cpp
+++ q8/8b.cpp
@@ -8,77 +8,57 @@ struct Edge {
     int src, dest, weight;
 };
 
-// Sort edges by weight
 bool compareEdge(Edge a, Edge b)
 {
     return a.weight < b.weight;
 }
 
-struct DisjointSet {
-    vector<int> parent, rank;
-    
-    DisjointSet(int n)
-    {
-        parent.resize(n);
-        rank.resize(n, 0);
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-    }
-    
-    int find(int i)
-    {
-        if (parent[i] == i)
-            return i;
-        return parent[i] = find(parent[i]);
-    }
-    
-    void unite(int x, int y)
-    {
-        int rootX = find(x);
-        int rootY = find(y);
-        
-        if (rootX != rootY) {
-            if (rank[rootX] < rank[rootY])
-                parent[rootX] = rootY;
-            else if (rank[rootX] > rank[rootY])
-                parent[rootY] = rootX;
-            else {
-                parent[rootY] = rootX;
-                rank[rootX]++;
-            }
-        }
-    }
-};
-
-void kruskalMST(vector<Edge>& edges, int V)
+int findParent(int i, vector<int>& parent)
 {
-    vector<Edge> result;
-    int minCost = 0;
-    
+    if (parent[i] == i) return i;
+
+    return parent[i] = findParent(parent[i], parent);
+}
+
+void kruskals(vector<Edge>& edges, int V)
+{
     sort(edges.begin(), edges.end(), compareEdge);
     
-    DisjointSet ds(V);
-    
-    for (auto edge : edges)
+    vector<int> parent(V);
+
+    for (int v=0; v<V; v++)
     {
-        int rootSrc = ds.find(edge.src);
-        int rootDest = ds.find(edge.dest);
+        parent[v] = v;
+    }
+
+    vector<Edge> result;
+
+    int minCost = 0;
+    
+    for(auto edge : edges)
+    {
+        int rootSrc = findParent(edge.src, parent);
+
+        int rootDest = findParent(edge.dest, parent);
         
-        if (rootSrc != rootDest)
+        if(rootSrc != rootDest)
         {
             result.push_back(edge);
+
             minCost += edge.weight;
-            ds.unite(rootSrc, rootDest);
+            
+            parent[rootSrc] = rootDest;
         }
     }
     
     cout << "Minimum Spanning Tree (Kruskal's Algorithm):" << endl;
     cout << "Edge \tWeight" << endl;
-    for (auto edge : result)
+
+    for(auto edge : result)
     {
         cout << edge.src << " - " << edge.dest << " \t" << edge.weight << endl;
     }
+    
     cout << "Minimum Total Cost: " << minCost << endl;
 }
 
@@ -94,7 +74,7 @@ int main()
         {2, 3, 4}
     };
     
-    kruskalMST(edges, V);
+    kruskals(edges, V);
     
     return 0;
 }
